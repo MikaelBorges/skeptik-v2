1c8c9119fb8732099d5563108bda2f68271e356a
Mikael B. <borges.mikael@gmail.com>
2024-02-01 15:55:43 +0100

    remove dev mode for production + fix suspense require on build when use useSearchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { DevMode } from "@/components/devMode/devMode";
 import { Filters } from "@/components/filters/filters";
 import { ProductList } from "@/components/productList/productList";
 
 export default function RootPage() {
   return (
     <main className="space-y-4 flex flex-col items-center p-4">
-      <DevMode />
       <Filters />
       <ProductList />
     </main>
